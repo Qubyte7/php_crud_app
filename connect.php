5e0c24bd543297067f047ba14a626e181f55dba2
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-05-01 22:33:39 +0200

    first commit

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
-$connection=new mysqli('localhost','root','','app1php');//the space left is because i don't have the password in my database
+$connection=new mysqli('localhost',
+'root','','app1php');//the space left is because i don't have the password in my database
 
 if(!$connection){
     die(mysqli_error($connection));
